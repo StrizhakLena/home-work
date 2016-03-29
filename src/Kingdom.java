f5cfdcb2d004ba5dd33f4f6472180aee9ae1d19e
StrizhakLena <strizhak_lena@mail.ru>
2016-03-29 21:10:20 +0300

    Update and rename Main.java to Kingdom.java

---
+++ src/Kingdom.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Strizhak on 23.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Kingdom a = new Kingdom();
+        String n = a.toString();
+        System.out.println(n);
+    }
+}
