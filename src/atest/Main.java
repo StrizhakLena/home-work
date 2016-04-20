655acbddda3c546a571edf46df6c4f381637823c
StrizhakLena <strizhak_lena@mail.ru>
2016-04-20 08:38:50 +0300

    Create Main.java

---
+++ src/atest/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by User on 06.04.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Exam exam = new Exam();
+
+        exam.setSubject("OOP");
+        exam.setMark(5);
+
+
+        System.out.println(exam.toString());
+    }
+}
